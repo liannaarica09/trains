5f62bea1022b4021e53eb42487c24cb334d93c66
Maureen Bockrath <maureenbockrath@Maureens-MacBook-Air.local>
2018-03-02 19:01:40 -0800

    grid created and Firebase initialized

---
+++ script.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAKsTGz-iujSCYaAHM6rJE5wrQb-Mi247U",
+    authDomain: "traintimes-9becd.firebaseapp.com",
+    databaseURL: "https://traintimes-9becd.firebaseio.com",
+    projectId: "traintimes-9becd",
+    storageBucket: "",
+    messagingSenderId: "658219272931"
+  };
+  firebase.initializeApp(config);
